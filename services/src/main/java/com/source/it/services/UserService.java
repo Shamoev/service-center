a183fe3457c699e4a43996a8a325e3088b2e01b1
Shamoev <wamoev@mail.ru>
2017-05-09 22:25:37 +0300

    lecture16 jdbc services

---
+++ services/src/main/java/com/source/it/services/UserService.java
@@ -0,0 +1,18 @@
+package com.source.it.services;
+
+import jdbc.exceptions.GenericDaoException;
+import jdbc.manager.AbstractManagerFactory;
+import jdbc.manager.UserManager;
+import jdbc.model.User;
+
+public class UserService {
+    private UserManager userManager = AbstractManagerFactory.getManagerFactory().getUserManager();
+
+    public User getUserByLogin(String login) {
+        try {
+            return userManager.getUserByLogin(login);
+        } catch (GenericDaoException e) {
+            return null;
+        }
+    }
+}
